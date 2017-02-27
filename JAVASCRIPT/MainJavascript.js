fdb4d835b94eb28c7111cb0775247f9c83d2b3d9
Won Yong Ha <hpmsora@gmail.com>
2017-02-27 12:31:28 -0500

    27 February 2017

--- JAVASCRIPT/MainJavascript.js
+++ JAVASCRIPT/MainJavascript.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 $(document).ready(function() {
     $('#content').load('/SECTIONS/HomeContents.php');
 
-    $('ul#main_navigation_id li a').click(function() {
+    $('#main_navigation_id li a').click(function() {
 	var pageName = $(this).attr('href');
 	alert(pageName);
 	$('#content').load('SECTIONS/' + pageName + '.php');
