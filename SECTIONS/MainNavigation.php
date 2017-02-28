77e19ef5873fb9fd238dabae2bb4f7c75427e33a
Won Yong Ha <hpmsora@gmail.com>
2017-02-28 16:00:32 -0500

    28 February 2017

--- SECTIONS/MainNavigation.php
+++ SECTIONS/MainNavigation.php
@@ -2,4 +2,10 @@
   <li id="navC1_id"><span><a>HOME</a></span></li>
   <li id="navC2_id"><span><a>ABOUT</a></span></li>
   <li id="navC3_id"><span><a>WORKS</a></span></li>
+  $("a").click(function() {
+	alert('Detect');
+	//var pageName = $(this).attr('href');
+	//alert(pageName);
+	//$('#content').load('SECTIONS/' + pageName + '.php');
+    });
 </ul>
