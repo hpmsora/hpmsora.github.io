9b29f2c96fc548179ed75a6d883380b75b733c0a
Won Yong Ha <hpmsora@gmail.com>
2017-02-28 18:32:19 -0500

    28 February 2017

--- SECTIONS/MainNavigation.php
+++ SECTIONS/MainNavigation.php
@@ -1,11 +1,11 @@
 <ul class="main_navigation_class" id="main_navigation_id">
-  <li id="navC1_id"><span><a href="HomeContents">HOME</a></span></li>
-  <li id="navC2_id"><span><a href="AboutContents">ABOUT</a></span></li>
-  <li id="navC3_id"><span><a href="WorksContents">WORKS</a></span></li>
+  <li id="navC1_id"><span><a data-type="HomeContents">HOME</a></span></li>
+  <li id="navC2_id"><span><a data-type="AboutContents">ABOUT</a></span></li>
+  <li id="navC3_id"><span><a data-type="WorksContents">WORKS</a></span></li>
 </ul>
   <script>
-  $("a").click(function() {
-	var pageName = $(this).attr('href');
+  $(".main_navigation_class li span a").click(function() {
+	var pageName = $(this).attr('data-type');
 	$('#content').load('SECTIONS/' + pageName + '.php');
     });
  </script>
