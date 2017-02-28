30374722553ffccf446d737d141f0baaab8fcf33
Won Yong Ha <hpmsora@gmail.com>
2017-02-28 15:59:29 -0500

    28 February 2017

--- JAVASCRIPT/MainJavascript.js
+++ JAVASCRIPT/MainJavascript.js
@@ -1,13 +1,10 @@
 $(document).ready(function() {
     $("#MainNavigationEmbedding").load("SECTIONS/MainNavigation.php");
     $('#content').load('/SECTIONS/HomeContents.php');
-});
-
-$(document).ready(function() {
-    $('.main_navigation_class li').on("click", function() {
+    $("a").click(function() {
 	alert('Detect');
-	var pageName = $(this).attr('href');
-	alert(pageName);
-	$('#content').load('SECTIONS/' + pageName + '.php');
+	//var pageName = $(this).attr('href');
+	//alert(pageName);
+	//$('#content').load('SECTIONS/' + pageName + '.php');
     });
 });
