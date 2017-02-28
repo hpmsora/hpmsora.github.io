247484cec973cec5b61b6879a7162ab45177b63a
Won Yong Ha <hpmsora@gmail.com>
2017-02-27 22:53:06 -0500

    27 February 2017

--- SECTIONS/MainNavigation.php
+++ SECTIONS/MainNavigation.php
@@ -1,5 +1,5 @@
 <ul class="main_navigation_class" id="main_navigation_id">
-  <li id="navC1_id"><a id="C1" href="HomeContents">HOME</a></li>
+  <li id="navC1_id"><a id="C1" href="javascript:void(0)">HOME</a></li>
   <li id="navC2_id"><a href="AboutContents">ABOUT</a></li>
   <li id="navC3_id"><a href="WorkContents">WORKS</a></li>
 </ul>
