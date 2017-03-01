bd2ced89c8dbdc7d013ca0b8158e6a6c6e25403e
Won Yong Ha <hpmsora@gmail.com>
2017-02-28 19:45:19 -0500

    28 February 2017

--- SECTIONS/MainNavigation.php
+++ SECTIONS/MainNavigation.php
@@ -1,7 +1,7 @@
 <ul class="main_navigation_class" id="main_navigation_id">
-  <li id="navC1_id"><span><a data-type="HomeContents">HOME</a></span></li>
-  <li id="navC2_id"><span><a data-type="AboutContents">ABOUT</a></span></li>
-  <li id="navC3_id"><span><a data-type="WorksContents">WORKS</a></span></li>
+  <li id="navC1_id"><span><a href="#" data-type="HomeContents">HOME</a></span></li>
+  <li id="navC2_id"><span><a href="#" data-type="AboutContents">ABOUT</a></span></li>
+  <li id="navC3_id"><span><a href="#" data-type="WorksContents">WORKS</a></span></li>
 </ul>
   <script>
   $(".main_navigation_class li span a").click(function() {
