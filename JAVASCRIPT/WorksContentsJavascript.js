a7c92771fe04b5a3ce50c5d52daccec8d9e2e3ba
Won Yong Ha <hpmsora@gmail.com>
2017-03-06 04:24:14 -0500

    6 March 2017

--- JAVASCRIPT/WorksContentsJavascript.js
+++ JAVASCRIPT/WorksContentsJavascript.js
@@ -1,26 +1,23 @@
 $(document).ready(function() {
 
-    var $contentsArray = [];
-
     $.ajax({
 	url:'https://api.github.com/users/hpmsora/repos',
 	type:'GET',
 	success: function(data){
-	    $contentsArray = data;
-	    console.log($contentsArray);
+	    $.each(data, function(index, value) {
+		console.log("BBBB");
+		console.log(index);
+		$('<div/>', {
+		    class: 'test_class',
+		    id: 'test_id',
+		    
+		    text: index + ": " + value.get('name')
+		}).appendTo('#content');
+	    });
 	}
     });
-
+    
     console.log("AAAAA");
     
-    $.each($contentsArray, function(index, value) {
-	console.log("BBBB");
-	console.log(index);
-	$('<div/>', {
-	    class: 'test_class',
-	    id: 'test_id',
-	    
-	    text: index + ": " + value.get('name')
-	}).appendTo('#content');
-    });
+    
 });
