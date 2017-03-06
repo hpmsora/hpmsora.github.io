3240d7b2a47ba9d405a96354dfcadc26d49c7307
Won Yong Ha <hpmsora@gmail.com>
2017-03-06 05:00:23 -0500

    6 March 2017

--- JAVASCRIPT/WorksContentsJavascript.js
+++ JAVASCRIPT/WorksContentsJavascript.js
@@ -6,15 +6,14 @@ $(document).ready(function() {
 	success: function(data){
 	    $.each(data, function(index, value) {
 		console.log(index);
-		$('<div/>', {
-		    class: 'test_class',
-		    id: 'test_id',
-		    
-		    text: $('<a/>', {
-			href: value.html_url,
-			text: (index + 1) + ": " + value.name
-		    })
-		}).appendTo('#content');
+		$('<a/>', {
+		    href: value.html_url,
+		    text: (index + 1) + ": " + value.name
+		}).appendTo(
+		    $('<div/>', {
+			class: 'test_class',
+			id: 'test_id'
+		    }).appendTo('#content'))
 	    });
 	}
     });
