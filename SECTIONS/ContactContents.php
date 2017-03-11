8e68a1aebbabea00e2afbff6025f8c98c004cb69
Won Yong Ha <hpmsora@gmail.com>
2017-03-11 16:23:34 -0500

    11 March 2017

--- SECTIONS/ContactContents.php
+++ SECTIONS/ContactContents.php
@@ -1,7 +1,9 @@
 <div class="contact_contents_wrap">
 <link rel="stylesheet" type="text/css" href="CSS/ContactContentsStyle.css">
-<div class="contactInnerWarp">
-<span>THE ART OF COMPUTER & CODING</span>
-<img class = "Contact_Image" src="ASSETS/ExampleImage.jpg">
-</div>
+	<div class="contactInnerWarp">
+		<div class="contactQuoteWrap">
+			<span>THE ART OF COMPUTER & CODING</span>
+			<img class = "Contact_Image" src="ASSETS/ExampleImage.jpg">
+		</div>
+	</div>
 </div>
