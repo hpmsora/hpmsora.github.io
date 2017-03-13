aa7e81ebb53f4df235e6d2524afab4ec886dbcf6
Won Yong Ha <hpmsora@gmail.com>
2017-03-13 04:10:37 -0400

    13 March 2017

--- JAVASCRIPT/AboutContentsJavascript.js
+++ JAVASCRIPT/AboutContentsJavascript.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
 		text: 'RESUME'
 	}).appendTo('.aboutResumeButtonInner_Button')
 	$.get('/ASSETS/Quotes.txt', function(data) {
-		$('.aboutQuoteInner_Quote').html(data)
+		var dataList = data.splot('$$$$')
+		$('.aboutQuoteInner_Quote').html(dataList[0])
 	})
 });
