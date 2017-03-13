ea31c2c15c4c209252761b4012d789e88406a126
Won Yong Ha <hpmsora@gmail.com>
2017-03-13 03:40:11 -0400

    13 March 2017

--- SECTIONS/AboutContents.php
+++ SECTIONS/AboutContents.php
@@ -1,6 +1,5 @@
 <div class="about_contents_wrap">
 	<link rel="stylesheet" type="text/css" href="CSS/AboutContentsStyle.css">
-	<script rel="text/javascript" src="JAVASCRIPT/AboutContentsJavascript.js"></script>
 	<div class="aboutInnerWrap">
 		<div class="aboutImageWrap">
 			Image
@@ -14,6 +13,7 @@
 		<div class="aboutResumeButtonWrap">
 			<div class="aboutResumeButtonInner_Up"></div>
 			<div class="aboutResumeButtonInner_Button">
+			<script rel="text/javascript" src="JAVASCRIPT/AboutContentsJavascript.js"></script>
 			</div>
 			<div class="aboutResumeButtonInner_Down"</div>
 		</div>
