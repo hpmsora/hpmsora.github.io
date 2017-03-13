0312bc3a08995f9955beb87b3c8ad6de90482111
Won Yong Ha <hpmsora@gmail.com>
2017-03-13 03:33:51 -0400

    13 March 2017

--- JAVASCRIPT/AboutContentsJavascript.js
+++ JAVASCRIPT/AboutContentsJavascript.js
@@ -4,5 +4,5 @@ $(document).ready(function() {
 		href: 'hpmsora.github.io/ASSETS/Resume - Won Yong Ha.pdf',
 		target: '_blank',
 		text: 'RESUME'
-	}).append('aboutResumeButtonInner_Button')
+	}).append('.aboutResumeButtonInner_Button')
 });
