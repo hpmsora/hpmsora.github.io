675d928b5738886b69733ed84fad041c000f39fa
Won Yong Ha <hpmsora@gmail.com>
2017-03-18 23:08:10 -0400

    18 March 2017

--- JAVASCRIPT/WorksContentsJavascript.js
+++ JAVASCRIPT/WorksContentsJavascript.js
@@ -41,6 +41,12 @@ $(document).ready(function() {
 			"-o-transition":"all 0.15s ease",
 			"-ms-transition":"all 0.15s ease",
 			"transition":"all 0.15s ease"
+		}).hover(function() {
+		$(this).css(
+			"-webkit-box-shadow":"1px 1px 1px rgba(0,0,0,.29), inset 0px 0px 2px rgba(0,0,0, .5)",
+			"-moz-box-shadow":"1px 1px 1px rgba(0,0,0,.29), inset 0px 0px 2px rgba(0,0,0, .5)",
+			"box-shadow":"1px 1px 1px rgba(0,0,0,.29), inset 0px 0px 2px rgba(0,0,0, .5)"
+		)
 		}).appendTo(
 		    $('<div/>', {
 			class: 'WorkContent_InnerWrap_class',
