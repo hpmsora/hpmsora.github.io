bbc0d78e3595dfe7763675812ac95d63db971e62
Won Yong Ha <hpmsora@gmail.com>
2017-04-16 23:32:29 -0400

    16 April 2017

--- JAVASCRIPT/MainJavascript.js
+++ JAVASCRIPT/MainJavascript.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
     $("#MainNavigationEmbedding").load("SECTIONS/MainNavigation.php");
     $('#MainContent').load('/SECTIONS/AboutContents.php');
-    $('#footer_wrap').load('/SECTIONS/FooterContents.php');
+    $('#main_footer_wrap').load('/SECTIONS/FooterContents.php');
 });
