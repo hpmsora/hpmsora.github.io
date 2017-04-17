0e0fec4ab5246cf86138c849bc59e8a82d3e2210
Won Yong Ha <hpmsora@gmail.com>
2017-04-17 00:14:41 -0400

    16 April 2017

--- SECTIONS/FooterContents.php
+++ SECTIONS/FooterContents.php
@@ -1,5 +1,8 @@
-<div class="FooterInnerWrap">
-  Copyright &copy; Won Yong Ha
-  <img src="ASSETS/Facebook_Icon.png" alt="Facebook Profile" height="30px" width="30px">
+<div class="footer_contents_wrap">
+  <div class="FooterConents_Text">Copyright &copy; Won Yong Ha</div>
+  <div class="FooterConents_Links">
+    <a href="https://www.facebook.com/wonyongha36"><img src="ASSETS/Facebook_Icon.png" alt="Facebook Profile" height="30px" width="30px"></a>
+    <a href="https://www.linkedin.com/in/wonyongha/"><img src="ASSETS/Linkedin_Icon.png" alt="LinkedIn Profile" height="30px" width="30px"></a>
+  </div>
 </div>
 <link rel="stylesheet" type="text/css" href="CSS/FooterContentsStyle.css"/>
