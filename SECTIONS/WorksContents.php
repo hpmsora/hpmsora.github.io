166673de6f101d8656da87f83af3d8f9200e1691
Won Yong Ha <hpmsora@gmail.com>
2017-04-16 23:16:18 -0400

    16 April 2017

--- SECTIONS/WorksContents.php
+++ SECTIONS/WorksContents.php
@@ -1,4 +1,4 @@
 <div class="work_contents_wrap">
    <script rel="text/javascript" src="JAVASCRIPT/WorksContentsJavascript.js"></script>
 </div>
-<link rel="stylesheet" type="text/css" href="CSS/WorksContentsStyle.css">
+<link rel="stylesheet" type="text/css" href="CSS/WorksContentsStyle.css"> 
