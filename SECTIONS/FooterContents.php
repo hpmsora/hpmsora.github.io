66ee7206d858065a4dc5b67d18f763436a205912
Won Yong Ha <hpmsora@gmail.com>
2017-04-17 00:02:29 -0400

    16 April 2017

--- SECTIONS/FooterContents.php
+++ SECTIONS/FooterContents.php
@@ -1,5 +1,5 @@
 <div class="FooterInnerWrap">
   Copyright &copy; Won Yong Ha
-  <img src="ASSET/Facebook_Icon.png" alt="Facebook Profile" height="30px" width="30px">
+  <img src="ASSETS/Facebook_Icon.png" alt="Facebook Profile" height="30px" width="30px">
 </div>
 <link rel="stylesheet" type="text/css" href="CSS/FooterContentsStyle.css"/>
