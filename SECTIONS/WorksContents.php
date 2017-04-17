2850e844c49ae35c0ab47ff3f07d7f21c78f7aa7
Won Yong Ha <hpmsora@gmail.com>
2017-04-16 20:52:20 -0400

    16 April 2017

--- SECTIONS/WorksContents.php
+++ SECTIONS/WorksContents.php
@@ -1,4 +1,3 @@
 <div class="work_contents_wrap">
    <script rel="text/javascript" src="JAVASCRIPT/WorksContentsJavascript.js"></script>
-   <script rel="text/javascript" src="JAVASCRIPT/WorksContentsFunctionsJavascript.js"></script>
 </div>
