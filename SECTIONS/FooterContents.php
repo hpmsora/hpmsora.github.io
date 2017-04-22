fb40c2ba13d2d99ea725fd337916c496ab2f1ee6
Won Yong Ha <hpmsora@gmail.com>
2017-04-22 03:05:54 -0400

    22 April 2017

--- SECTIONS/FooterContents.php
+++ SECTIONS/FooterContents.php
@@ -1,5 +1,5 @@
 <div class="footer_contents_wrap">
-  <span>Copyright &copy; Won Yong</span>
+  <span>Copyright &copy; Won Yong Ha</span>
   <span>
     <a href="https://www.facebook.com/wonyongha36"><img src="ASSETS/Facebook_Icon.png" alt="Facebook Profile" height="20px" width="20px"></a>
     <a href="https://www.linkedin.com/in/wonyongha/"><img src="ASSETS/Linkedin_Icon.png" alt="LinkedIn Profile" height="20px" width="20px"></a>
