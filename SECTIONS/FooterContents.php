35d7bfc549fcb5096605b28d0a50c2b174bbb29b
Won Yong Ha <hpmsora@gmail.com>
2017-04-24 12:40:41 -0400

    24 April 2017

--- SECTIONS/FooterContents.php
+++ SECTIONS/FooterContents.php
@@ -2,7 +2,9 @@
   <span>Copyright &copy; Won Yong Ha</span>
   <span>
     <a href="https://www.facebook.com/wonyongha36"><img src="ASSETS/Facebook_Icon.png" alt="Facebook Profile" height="20px" width="20px"></a>
+    <a href="https://www.instagram.com/hawonyong/"><img src="ASSETS/Instagram_Icon.png" alt="LinkedIn Profile" height="20px" width="20px"></a>
     <a href="https://www.linkedin.com/in/wonyongha/"><img src="ASSETS/Linkedin_Icon.png" alt="LinkedIn Profile" height="20px" width="20px"></a>
+    <a href="https://github.com/hpmsora"><img src="ASSETS/Github_Icon.png" alt="LinkedIn Profile" height="20px" width="20px"></a>
   </span>
 </div>
 <link rel="stylesheet" type="text/css" href="CSS/FooterContentsStyle.css"/>
