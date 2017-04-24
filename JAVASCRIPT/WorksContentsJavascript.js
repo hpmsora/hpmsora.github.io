30282bc08a896152bc40478995bb3070996200f9
Won Yong Ha <hpmsora@gmail.com>
2017-04-24 01:49:46 -0400

    24 April 2017

--- JAVASCRIPT/WorksContentsJavascript.js
+++ JAVASCRIPT/WorksContentsJavascript.js
@@ -45,9 +45,9 @@ $(document).ready(function() {
 					"position":"relative",
 					"text-align":"center",
 					"text-decoration":"none",
-					"font":"bold 12px Arial, sans-serif",
+					"font":"bold 15px Arial, sans-serif",
 			
-					//"text-shadow":"1px 1px 1px rgba(255,255,255, .22)",
+					"text-shadow":"1px 1px 1px rgba(255,255,255, .22)",
 			
 					"-webkit-box-shadow":"1px 1px 1px rgba(0,0,0, .29), inset 1px 1px 1px rgba(255,255,255, .44)",
 					"-moz-box-shadow":"1px 1px 1px rgba(0,0,0, .29), inset 1px 1px 1px rgba(255,255,255, .44)",
