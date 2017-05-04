cc28e55c568ccb8806713989db095fad2d4a03d6
Won Yong Ha <hpmsora@gmail.com>
2017-05-03 23:46:22 -0400

    May 3 2017 - Contact page LogoImage change function debugging

--- JAVASCRIPT/ContactContentsJavascript.js
+++ JAVASCRIPT/ContactContentsJavascript.js
@@ -7,9 +7,10 @@ function showContact() {
 }
 
 function LogoImageClick() {
-  if(document.getElementById('Contact_Image_id').src == 'ASSETS/LogoImage_1.png')
+  if(document.getElementById('Contact_Image_id').src == 'ASSETS/LogoImage_1.png') {
     console.log(document.getElementById('Contact_Image_id'))
     document.getElementById('Contact_Image_id').src = 'ASSETS/LogoImage_2.png'
+  }
   else
     document.getElementById('Contact_Image_id').src = 'ASSETS/LogoImage_1.png'
 }
