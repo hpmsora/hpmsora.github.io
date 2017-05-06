5eb64bfb7d151ccb416cc1fff4a228e86fee2632
Won Yong Ha <hpmsora@gmail.com>
2017-05-06 02:10:17 -0400

    May 6 2017 - Contact page id data fixed

--- JAVASCRIPT/ContactContentsJavascript.js
+++ JAVASCRIPT/ContactContentsJavascript.js
@@ -1,17 +1,18 @@
 function showContact() {
     if(document.getElementById("contactContactSessionWarp_id").style.display == "none") {
       document.getElementById("contactContactSessionWarp_id").style.display = "block";
-      console.log($('#Contact_Image_id').data("status"))
     } else {
     	document.getElementById("contactContactSessionWarp_id").style.display = "none";
     }
 }
 
 function LogoImageClick() {
-  if(document.getElementById('Contact_Image_id').src == 'ASSETS/LogoImage_1.png') {
-    console.log(document.getElementById('Contact_Image_id'))
+  if($('#Contact_Image_id').data("status") == 'LogoImage_1.png') {
     document.getElementById('Contact_Image_id').src = 'ASSETS/LogoImage_2.png'
+    $('#Contact_Image_id').data("status") = 'LogoImage_2.png'
   }
-  else
+  else {
     document.getElementById('Contact_Image_id').src = 'ASSETS/LogoImage_1.png'
+    $('#Contact_Image_id').data("status") = 'LogoImage_1.png'
+  }
 }
