eafb8829cf218860a8f26a4a4b9226a3083f786a
Won Yong Ha <hpmsora@gmail.com>
2017-05-07 01:09:27 -0400

    May 7 2017 - MainDecoration imporve

--- JAVASCRIPT/MainJavascript.js
+++ JAVASCRIPT/MainJavascript.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     $("#MainNavigationEmbedding").load("SECTIONS/MainNavigation.php");
+    $("#MainDecoration").load("SECTIONS/MainDecoration.php");
     $('#MainContent').load('SECTIONS/AboutContents.php');
     $('#main_footer_wrap').load('SECTIONS/FooterContents.php');
 });
