bdcd045bdc9747b368b0333a0b3fc70e0b8f03da
Won Yong Ha <hpmsora@gmail.com>
2017-05-07 01:20:14 -0400

    May 7 2017 - MainDecoration width check

--- SECTIONS/MainDecoration.php
+++ SECTIONS/MainDecoration.php
@@ -1,2 +1,5 @@
-<canvas>
+<canvas id="main_decoration_warp_id" class="main_decoration_warp">
 </canvas>
+
+<script rel="text/javascript" src="/JAVASCRIPT/MainDecorationJavascript.js"></script>
+<link rel="stylesheet" type="text/css" href="/CSS/MainDecorationStyle.css">
