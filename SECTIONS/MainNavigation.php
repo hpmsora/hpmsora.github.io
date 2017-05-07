c36f2ef99b33a1284459fba931e357a6a90c7b38
Won Yong Ha <hpmsora@gmail.com>
2017-05-07 02:33:57 -0400

    May 7 2017 - Decoration Session canvas z index

--- SECTIONS/MainNavigation.php
+++ SECTIONS/MainNavigation.php
@@ -1,6 +1,11 @@
 <ul class="main_navigation_class" id="main_navigation_id">
-  <li id="navC2_id"><span><a href="#" data-type="AboutContents">ABOUT</a></span></li>
-  <li id="navC3_id"><span><a href="#" data-type="WorksContents">WORKS</a></span></li>
+  <li id="navC3_id"><span><a href="#" data-type="AboutContents">ABOUT</a></span></li>
+  <li id="navC2_id"><span><a href="#" data-type="WorksContents">WORKS</a></span></li>
   <li id="navC1_id"><span><a href="#" data-type="ContactContents">CONTACT</a></span></li>
 </ul>
+<div>
+  <div id="navC3Mobile_id" class="navCMobile"></div>
+  <div id="navC2Mobile_id" class="navCMobile"></div>
+  <div id="navC1Mobile_id" class="navCMobile"></div>
+</div>
 <script rel="text/javascript" src="JAVASCRIPT/MainNavigationJavascript.js"></script>
