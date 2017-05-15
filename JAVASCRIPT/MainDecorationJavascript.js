67d183d02f1d89f984e4de1571a9b79d869d08fe
Won Yong Ha <hpmsora@gmail.com>
2017-05-14 23:56:19 -0400

    Canvas javascript check

--- JAVASCRIPT/MainDecorationJavascript.js
+++ JAVASCRIPT/MainDecorationJavascript.js
@@ -1,4 +1,13 @@
+var windowWidth = window.innerWidth;
+var windowHeight = window.innerHeight;
 var canvas = document.getElementById("main_decoration_warp_id");
-var ctx = canvas.getContext("2d");
-ctx.fillStyle = "#FF0000";
-ctx.fillRect(0,0,150,75);
+var context = canvas.getContext("2d");
+
+context.beginPath();
+context.moveTo(100, 100);
+context.lineTo(100, 300);
+context.lineTo(300, 300);
+context.closePath();
+
+context.fillStyle = "#FFCC00";
+context.fill();
