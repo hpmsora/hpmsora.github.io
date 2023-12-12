8c9c1edd2c21cfbe181f605b06953e56885aa169
Won Yong Ha <hpmsora@gmail.com>
2023-12-12 14:20:36 -0500

    Won Yong Ha: Fixed index
    Won Yong Ha 20231212: Fixed index.html problem

--- JAVASCRIPT/AboutContentsJavascript.js
+++ JAVASCRIPT/AboutContentsJavascript.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	$('<a/>', {
 		class: 'aboutResumeButton',
-		href: 'hpmsora.github.io/ASSETS/Resume - Won Yong Ha.pdf',
+		href: '/ASSETS/ResumeWonYongHa.pdf',
 		target: '_blank',
 		text: 'RESUME'
 	}).appendTo('.aboutResumeButtonInner_Button')
