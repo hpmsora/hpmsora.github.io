6636ce0316a7be4b100f933d20126807ccd3e9cf
Won Yong Ha <hpmsora@gmail.com>
2023-12-13 19:38:05 -0500

    Won Yong Ha: typo
    Won Yong Ha 20231213: Edit typo

--- JAVASCRIPT/PublicationsContentsJavascript.js
+++ JAVASCRIPT/PublicationsContentsJavascript.js
@@ -8,12 +8,12 @@ $(document).ready(function() {
 		for (index = 0; index < dataList.length; index++) {
 			$('<span/>', {
 				class: 'publicationList',
-				text: dataList[0]
+				text: dataList[index]
 			}).css({
 				"text-align":"center",
 				"font":"bold 15px/17px Arial, sans-serif",
 				"white-space":"normal"
-		}).appendTo('.listInner_list')
+			}).appendTo('.listInner_list')
 		}
 	})
 });
