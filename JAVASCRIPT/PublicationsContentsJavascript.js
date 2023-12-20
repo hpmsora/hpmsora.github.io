1f93162cea541e885832e8bb3745e68bc661fd7c
Won Yong Ha <hpmsora@gmail.com>
2023-12-20 14:01:09 -0500

    publication update

--- JAVASCRIPT/PublicationsContentsJavascript.js
+++ JAVASCRIPT/PublicationsContentsJavascript.js
@@ -2,13 +2,19 @@ $(document).ready(function() {
 	// Publication List Section
 	$.get('/ASSETS/Publications.txt', function(data) {
 		var dataList = data.split('$$$$');
-		var newDataList = []
+		var newDataList = [];
 		var index;
 
 		for (index = dataList.length - 1; index >= 0 ; index--) {
-			$('<span/>', {
+			paperInfoList = dataList[index].split('$');
+			paperTitle = paperInfoList[0];
+			paperAuthors = paperInfoList[1];
+			paperPublication = paperInfoList[2];
+
+			$('<p/>', {
 				class: 'publicationList',
-				text: dataList[index]
+				//text: dataList[index],
+				html: paperTitle + "<em>" + paperAuthors + "</em>" + paperPublication
 			}).css({
 				"text-align":"center",
 				"font":"bold 15px/17px Arial, sans-serif",
