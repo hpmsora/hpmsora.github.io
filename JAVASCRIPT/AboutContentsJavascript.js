4dddd2f90660051d0023577aefb098a0a6bcfee9
Won Yong Ha <hpmsora@gmail.com>
2023-12-23 18:33:43 -0500

    Update AboutContentsJavascript.js

--- JAVASCRIPT/AboutContentsJavascript.js
+++ JAVASCRIPT/AboutContentsJavascript.js
@@ -64,7 +64,8 @@ $(document).ready(function() {
 		}).css({
 			"text-align":"center",
 			"font":"bold 15px/17px Arial, sans-serif",
-			"white-space":"normal"
+			"white-space":"normal",
+			"margin" : "0 auto"
 		}).appendTo('.aboutQuoteInner_Quote_Quote')
 
 		$('<span/>', {
@@ -73,7 +74,8 @@ $(document).ready(function() {
 		}).css({
 			"text-align":"center",
 			"font":"bold 15px/17px Arial, sans-serif",
-			"white-space":"normal"
+			"white-space":"normal",
+			"margin" : "0 auto"
 		}).appendTo('.aboutQuoteInner_Quote_Authors')
 	})
 });
