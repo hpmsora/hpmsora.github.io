1e33eb0481248c25deced9eaac9550c24cdd8290
Won Yong Ha <hpmsora@gmail.com>
2024-01-02 02:23:53 -0500

    fix

--- SECTIONS/AboutContents.php
+++ SECTIONS/AboutContents.php
@@ -1,6 +1,10 @@
 <div class="about_contents_wrap">
 	<script src="https://unpkg.com/github-calendar@latest/dist/github-calendar.min.js"></script>
 	<script rel="text/javascript" src="JAVASCRIPT/AboutContentsJavascript.js"></script>
+	<script type="text/javascript">
+		
+		GitHubCalendar(".aboutGitActivitiesWrap", "hpmsora", { responsive: true });
+	</script>
 	<link rel="stylesheet" href="https://unpkg.com/github-calendar@latest/dist/github-calendar-responsive.css"/>
 	<link rel="stylesheet" type="text/css" href="CSS/AboutContentsStyle.css"/>
 	<div class="aboutInnerWrap">
