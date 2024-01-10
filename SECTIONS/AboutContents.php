ceae224201509e0ed56ae520ea4f4548a0faaf02
Won Yong Ha <hpmsora@gmail.com>
2024-01-10 18:24:49 -0500

    Update AboutContents.php

--- SECTIONS/AboutContents.php
+++ SECTIONS/AboutContents.php
@@ -1,3 +1,4 @@
+<?php
 <div class="about_contents_wrap">
 	<div class="aboutInnerWrap">
 		<div class="aboutNameWrap">
@@ -44,3 +45,4 @@
 	<script rel="text/javascript" src="JAVASCRIPT/AboutContentsJavascript.js"></script>
 	<link rel="stylesheet" type="text/css" href="CSS/AboutContentsStyle.css"/>
 </div>
+?>
