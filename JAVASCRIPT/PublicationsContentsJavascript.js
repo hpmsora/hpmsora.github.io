63375a01c02a84e41f9e3128bfb1843b93e97773
Won Yong Ha <hpmsora@gmail.com>
2024-01-10 18:17:46 -0500

    Project list update

--- JAVASCRIPT/PublicationsContentsJavascript.js
+++ JAVASCRIPT/PublicationsContentsJavascript.js
@@ -45,4 +45,37 @@ $(document).ready(function() {
 	}).css({
 		"font-size" : "30px"
 	}).appendTo('.project_titleInner_title')
+
+	// Project List Section
+	$.get('/ASSETS/PROJECTS/Projects.txt', function(data) {
+		var dataList = data.split('$$$$');
+		var newDataList = [];
+		var index;
+
+		for (index = dataList.length - 1; index >= 0 ; index--) {
+			projectInfoList = dataList[index].split('$');
+			projectTitle = projectInfoList[0];
+			projectInstitute = projectInfoList[1];
+			projectDate = projectInfoList[2];
+			projectLink = projectInfoList[3];
+
+			paperInfo = "<a style='text-decoration: none; color: inherit' href=/ASSETS/PROJECTS/" + projectLink + ">" + "<span style='font-weight:bold'> " + projectTitle + "</span></a>" +  "<span style='font-style:italic'>" + ", " +  projectInstitute + "</span>" + 	"<span>" + ", " + projectDate + "." + "</span>";
+
+			$('<p/>', {
+				class: 'projectList',
+				html: paperInfo
+			}).css({
+				"text-align":"left",
+				"font":"15px/17px Arial, sans-serif",
+				"white-space":"normal"
+			}).appendTo(
+				$('<div/>', {
+					id: 'project' + index
+				}).css({
+					"width" : "100%",
+					"margin-bottom" : "30px"
+				}).appendTo('.project_listInner_list')
+			)
+		}
+	})
 });
