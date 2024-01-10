1f15db4040f27caac96a53d7afae951bc33bdc5c
Won Yong Ha <hpmsora@gmail.com>
2024-01-10 17:50:52 -0500

    paper title updated

--- SECTIONS/PublicationsContents.php
+++ SECTIONS/PublicationsContents.php
@@ -3,7 +3,9 @@
    <link rel="stylesheet" type="text/css" href="CSS/PublicationsContentsStyle.css">
    <div class="publicationsInnerWrap">
       <div class="paper_listWrap">
-         <div class="paper_title"></div>
+         <div class="paper_titleInner_Up"></div>
+         <div class="paper_titleInner_title"></div>
+         <div class="paper_titleInner_Down"></div>
          <div class="paper_listInner_Up"></div>
          <div class="paper_listInner_list"></div>
          <div class="paper_listInner_Down"></div>
