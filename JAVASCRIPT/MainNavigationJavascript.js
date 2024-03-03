674526dea3f58e13b84ecd6a771deca8d0043dfb
Won Yong Ha <hpmsora@gmail.com>
2024-03-03 01:56:39 -0500

    Update MainNavigationJavascript.js

--- JAVASCRIPT/MainNavigationJavascript.js
+++ JAVASCRIPT/MainNavigationJavascript.js
@@ -1,3 +1,7 @@
+$(".navC1_id span a").css(
+  "color" : "#858585"
+);
+
 $(".main_navigation_class li span a").click(function() {
   var pageName = $(this).attr('data-type');
   $('#MainContent').load('SECTIONS/' + pageName + '.php');
