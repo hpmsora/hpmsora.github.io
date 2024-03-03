9c6c26144e209cbdbc4cb04368a139abeb14e591
Won Yong Ha <hpmsora@gmail.com>
2024-03-03 02:08:50 -0500

    update

--- JAVASCRIPT/MainNavigationJavascript.js
+++ JAVASCRIPT/MainNavigationJavascript.js
@@ -1,14 +1,17 @@
 $("#navC1_id span a").css({
-  "color" : "#2B2B2B"
+  "color" : "#2B2B2B",
+  "text-decoration" : "underline"
 });
 
 $(".main_navigation_class li span a").click(function() {
   var pageName = $(this).attr('data-type');
   $('#MainContent').load('SECTIONS/' + pageName + '.php');
   $(".main_navigation_class li span a").css({
-    "color" : "#CFCFCF"
+    "color" : "#B1B1B1"
+    "text-decoration" : "none"
   });
   $(this).css({
     "color" : "#2B2B2B"
+    "text-decoration" : "underline"
   });
 });
