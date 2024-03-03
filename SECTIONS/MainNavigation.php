e77f11b2b89f711d13b3ab00a1d52cd61ed658b9
Won Yong Ha <hpmsora@gmail.com>
2024-03-03 01:35:08 -0500

    Update MainNavigation.php

--- SECTIONS/MainNavigation.php
+++ SECTIONS/MainNavigation.php
@@ -1,8 +1,8 @@
 <ul class="main_navigation_class" id="main_navigation_id">
-  <li id="navC4_id"><span><a href="#" data-type="ContactContents">CONTACT</a></span></li>
-  <li id="navC3_id"><span><a href="#" data-type="WorksContents">ARCHIVE</a></span></li>
-  <li id="navC2_id"><span><a href="#" data-type="PublicationsContents">PUBLICATIONS</a></span></li>
-  <li id="navC1_id"><span><a href="#" data-type="AboutContents">ABOUT</a></span></li>
+  <li id="navC4_id"><span><a href="#" data-type="ContactContents">Contact</a></span></li>
+  <li id="navC3_id"><span><a href="#" data-type="WorksContents">Archive</a></span></li>
+  <li id="navC2_id"><span><a href="#" data-type="PublicationsContents">Publications</a></span></li>
+  <li id="navC1_id"><span><a href="#" data-type="AboutContents">About</a></span></li>
 </ul>
 <div>
   <div id="navC1Mobile_id" class="navCMobile"></div>
